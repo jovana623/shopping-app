6b587cf8c73f8a6ca3c3cc2c46e28725e4c97e29
Jovana <jovanadjurdjevac08@gmail.com>
2024-07-10 16:33:33 +0200

    Checkout page fix

--- src/app/success/success.component.ts
+++ src/app/success/success.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { CartItem } from '../cart-item';
 import { CartService } from '../cart.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-success',
@@ -11,10 +12,15 @@ export class SuccessComponent {
   cartItems: CartItem[] = [];
   totalPrice: number = 0;
 
-  constructor(private cartService: CartService) {}
+  constructor(private cartService: CartService, private router: Router) {}
 
   ngOnInit(): void {
     this.cartItems = this.cartService.getCartItems();
     this.totalPrice = this.cartService.getTotalPrice();
+  }
+
+  clearCartAndNavigate(): void {
+    this.cartService.clearCart();
+    this.router.navigateByUrl('/products');
   }
 }
